c0525ee8265032a5ba09bb7162899fcfd886182e
kiki@happyflower.e4ward.com <kiki@happyflower.e4ward.com>
2024-02-18 00:35:14 +0000

    dbt homework - fhv data

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_external_yellow_trip_data.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_external_yellow_trip_data.sql
@@ -46,7 +46,7 @@ select
 from tripdata
 where rn = 1
 
--- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+-- dbt build --select <model_name> --vars '{"is_test_run": false}'
 {% if var('is_test_run', default=true) %}
     limit 100
 {% endif %}
