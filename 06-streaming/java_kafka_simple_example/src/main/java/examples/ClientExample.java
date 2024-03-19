4811461e33916f83655416d11bba44e99083b1dd
Lily Chau <chauyeeki@gmail.com>
2024-03-19 19:55:40 +0000

    Add Week 6 homework and relevant files

---
+++ 06-streaming/java_kafka_simple_example/src/main/java/examples/ClientExample.java
@@ -0,0 +1,19 @@
+// package example;
+
+// import java.lang.System;
+// import java.io.*;
+// import java.nio.file.*;
+// import java.util.*;
+// import java.time.*;
+
+// import org.apache.kafka.clients.consumer.*;
+// import org.apache.kafka.clients.producer.*; 
+// import org.apache.kafka.common.serialization.*; 
+
+
+// public class ClientExample {
+//     public static Properties readConfig(final String configFile) throws IOException {
+//         // reads the client configuration from client.properties
+
+//     }
+// }
